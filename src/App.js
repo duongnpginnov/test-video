5b9bdbe0883e76b0f76b98b5a587a380688c784e
duongnpGinnov <duongnp@ginnov.tech>
2021-10-20 13:57:26 +0700

    initial code

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { VideoSDKMeeting } from "@videosdk.live/rtc-js-prebuilt";
+
+export default function App() {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_VIDEOSDK_API_KEY;
+    const meetingId = "milkyway";
+    const name = "Demo User";
+
+    const config = {
+      name: name,
+      meetingId: meetingId,
+      apiKey: apiKey,
+      maxResolution: "sd",
+
+      containerId: null,
+      redirectOnLeave: "https://www.videosdk.live/",
+
+      micEnabled: true,
+      webcamEnabled: true,
+      participantCanToggleSelfWebcam: true,
+      participantCanToggleSelfMic: true,
+
+      chatEnabled: true,
+      screenShareEnabled: true,
+      pollEnabled: true,
+      whiteBoardEnabled: true,
+      raiseHandEnabled: true,
+
+      recordingEnabled: true,
+      recordingEnabledByDefault: false,
+      recordingWebhookUrl: "https://www.videosdk.live/callback",
+      participantCanToggleRecording: true,
+
+      brandingEnabled: true,
+      brandLogoURL: "https://picsum.photos/200",
+      brandName: "Awesome startup",
+
+      participantCanLeave: true, // if false, leave button won't be visible
+
+      livestream: {
+        autoStart: true,
+        outputs: [
+          // {
+          //   url: "rtmp://x.rtmp.youtube.com/live2",
+          //   streamKey: "<STREAM KEY FROM YOUTUBE>",
+          // },
+        ],
+      },
+
+      permissions: {
+        askToJoin: false, // Ask joined participants for entry in meeting
+        toggleParticipantMic: true, // Can toggle other participant's mic
+        toggleParticipantWebcam: true, // Can toggle other participant's webcam
+      },
+      joinScreen: {
+        visible: true, // Show the join screen ?
+        title: "Videosdk Demo", // Meeting title
+        meetingUrl:
+          window.location.pathname !== "/"
+            ? window.location.href
+            : window.location.host + "/videosdk-demo", // Meeting joining url
+      },
+    };
+
+    const meeting = new VideoSDKMeeting();
+    meeting.init(config);
+  }, []);
+
+  return <div></div>;
+}
